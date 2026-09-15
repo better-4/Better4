68524641f1bce901c2069cd61db719712d40896f
claire <claireplane@hotmail.com>
2026-09-15 17:45:15 -0400

    added college wallrides (#112)

--- src/load.c
+++ src/load.c
@@ -82,6 +82,7 @@ void patchLoad() {
 
 	// add_path_override("scripts\\qdir.txt", "scripts\\better4\\qdir.txt");
 	add_path_override("levels\\hof\\hof.col.Xbx", "levels\\better4\\hof\\hof.col.Xbx");
+	add_path_override("levels\\sch\\sch.col.Xbx", "levels\\better4\\sch\\sch.col.Xbx");
 
 	add_path_override("levels\\LA\\LA.col.Xbx", "levels\\better4\\LA\\LA.col.Xbx");
 	add_path_override("levels\\LA\\LA.scn.Xbx", "levels\\better4\\LA\\LA.scn.Xbx");
